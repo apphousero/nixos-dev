539bcc00db05c693f752559333298f26ca07ddf7
Andrei Floroiu <floroiu.andrew@gmail.com>
2025-12-18 21:03:24 +0200

    Added two more bindings for tmux popup windows

--- modules/tmux/default.nix
+++ modules/tmux/default.nix
@@ -58,6 +58,8 @@ in
 
         # lazygit popup window
         bind C-g display-popup -w 85% -h 85% -d '#{pane_current_path}' -E 'lazygit'
+        bind -n C-t display-popup -w 85% -h 85% -d '#{pane_current_path}' -E 'zsh'
+        bind -n C-x display-popup -w 85% -h 85% -d '#{pane_current_path}' -E 'bash'
 
         # Send the bracketed paste mode when pasting
         bind ] paste-buffer -p
