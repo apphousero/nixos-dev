fca66aba55e1c9019149998f6ac7c6c55ddde9a7
Andrei Floroiu <floroiu.andrew@gmail.com>
2026-02-01 17:06:53 +0200

    Feat/f20260201 switch to determinate nix (#15)
    * Switched to flakehub and determinate nix
    
    * Added missing but useful packages

--- modules/base.nix
+++ modules/base.nix
@@ -27,10 +27,6 @@
     settings = {
       auto-optimise-store = true;
 
-      experimental-features = [
-        "nix-command"
-        "flakes"
-      ];
       # Trusted users for binary cache
       trusted-users = [
         "root"
@@ -41,11 +37,13 @@
       substituters = [
         "https://cache.nixos.org/"
         "https://nix-community.cachix.org"
+        "https://install.determinate.systems"
       ];
 
       trusted-public-keys = [
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "cache.flakehub.com-3:hJuILl5sVK4iKm86JzgdXW12Y2Hwd5G07qKtHTOcDCM="
       ];
     };
   };
@@ -92,6 +90,8 @@
     git
     # GNU make
     gnumake
+    # Hetzber Cloud
+    hcloud
     # htop
     htop
     # JSON support in shell
@@ -142,6 +142,8 @@
     strace
     # tree command
     tree
+    # TCP dump
+    tcpdump
     # To complete a shell, ya need tmux
     tmux
     # Unzip
@@ -271,8 +273,8 @@
   };
 
   documentation = {
-    enable = lib.mkDefault true;
-    nixos.enable = lib.mkDefault true;
+    enable = lib.mkDefault false;
+    nixos.enable = lib.mkDefault false;
     man.enable = lib.mkDefault true;
   };
   # Allow unfree packages
