8515447e207a31578ce8877ecbfa2747e84ff9a3
Andrei Floroiu <floroiu.andrew@gmail.com>
2026-05-04 23:40:07 +0300

    Feat/f20260504 more updates (#53)
    * Add pi agent to home modules
    
    * Updated pi agent

--- packages/pi.nix
+++ packages/pi.nix
@@ -2,16 +2,16 @@
 
 pkgs.buildNpmPackage {
   pname = "pi";
-  version = "0.72.1";
+  version = "0.73.0";
 
   src = pkgs.fetchFromGitHub {
     owner = "badlogic";
     repo = "pi-mono";
-    rev = "036bde0a5db25f4ac55525a7b3d15a90717f737d";
-    hash = "sha256-SqUxghc60P3HfmaFJGB/m23mvzw0cD7cDEUrNFOqo0Y=";
+    rev = "dbcb473d6fdb96f60570b9ebe73e7aa6316fa8fb";
+    hash = "sha256-oE4zMH5KEH185Vdp0CE221sa9rJJw35jFLlfhTa3Sg4=";
   };
 
-  npmDepsHash = "sha256-KUC1xQK6oJXtg962YeLOnO76uTdR10/VNa9iiCdT3VM=";
+  npmDepsHash = "sha256-rBlAzAnP9aif1tZ984AO4HftIJsDgLQ+02J3td4jcRg=";
 
   nodejs = pkgs.nodejs_22;
 
