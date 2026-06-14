2c61a3069832cdb2f4728e24563f9704fce3cc37
Andrei Floroiu <floroiu.andrew@gmail.com>
2026-06-14 21:36:28 +0300

    Fixed build

--- modules/lib.nix
+++ modules/lib.nix
@@ -1,8 +1,5 @@
-{ config }:
+{ packages }:
 let
-  packages =
-    (if config ? environment then config.environment.systemPackages or [ ] else [ ])
-    ++ (if config ? home then config.home.packages or [ ] else [ ]);
   hasPackage = pkg: builtins.any (p: p.pname or p.name or "" == pkg) packages;
 in
 {
